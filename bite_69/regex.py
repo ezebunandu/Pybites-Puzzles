4406750cc9760cc97d78d050594e063fdba6d4a2
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-09-01 19:44:29 -0600

    One test still failing in bite 69

--- bite_69/regex.py
+++ bite_69/regex.py
@@ -28,20 +28,24 @@ def split_string_on_punctuation(text):
     """Split on ?!.,; - e.g. "hi, how are you doing? blabla" ->
        ['hi', 'how are you doing', 'blabla']
        (make sure you strip trailing spaces)"""
-    pass
+    split_text = re.split(r'[?!.,;]', text)
+    return [text.strip() for text in split_text if text]
 
 
 def remove_duplicate_spacing(text):
     """Replace multiple spaces by one space"""
-    pass
+    return re.sub(r'\s{2,}', ' ', text)
 
 
 def has_three_consecutive_vowels(word):
     """Returns True if word has at least 3 consecutive vowels"""
-    pass
+    return bool(re.search(r'[aeiou]{3,}', word))
 
 
 def convert_emea_date_to_amer_date(date):
     """Convert dd/mm/yyyy (EMEA date format) to mm/dd/yyyy
        (AMER date format)"""
-    pass
+    if re.match(r'\d{2}/\d{2}/\d{4}', date):
+        date, month, year = date.split('/')
+        date = f"{month}/{date}/{year}"
+    return date
