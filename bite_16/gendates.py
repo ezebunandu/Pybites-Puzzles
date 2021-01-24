ad74788607f1fd3fb3959657d527833fc68496e0
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-01-24 09:39:26 -0700

    bite 16: passes test

--- bite_16/gendates.py
+++ bite_16/gendates.py
@@ -4,6 +4,7 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    for i in range(1, 1_000_000):
-        yield from (PYBITES_BORN + timedelta(days=100 * i))
-        yield from (PYBITES_BORN + timedelta(days=365 * i))
+    start = PYBITES_BORN
+    while True:
+        start += timedelta(days=100)
+        yield start 
